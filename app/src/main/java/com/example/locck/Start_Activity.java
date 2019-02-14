7e3caa423bf174fa4aafb0a88f50127afe770960
Abhiram <abhiramareddy20@gmail.com>
2019-02-14 16:02:48 +0530

    image upload

--- app/src/main/java/com/example/locck/Start_Activity.java
+++ app/src/main/java/com/example/locck/Start_Activity.java
@@ -28,6 +28,9 @@ import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Start_Activity extends AppCompatActivity {
@@ -36,10 +39,12 @@ public class Start_Activity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
     private DatabaseReference UsersRef;
-    private static final int CAMERA_REQUEST_CODE = 1;
-    private StorageReference UserProfileImagesRef;
+    private static final int GALLERY_PICK = 1;
+    private StorageReference PostImagesRef;
     private ProgressDialog loadingBar;
     private ImageView img;
+    private Uri ImageUri;
+    private  String saveCurrentDate,saveCurrentTime,postRandomName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,18 +55,20 @@ public class Start_Activity extends AppCompatActivity {
         UploadPic = (Button) findViewById (R.id.button);
         update = (Button)findViewById (R.id.Update);
 
-        mAuth = FirebaseAuth.getInstance ();
+        /*mAuth = FirebaseAuth.getInstance ();
         currentUserID = mAuth.getCurrentUser ().getUid ();
         RootRef = FirebaseDatabase.getInstance ().getReference ();
-        UsersRef = FirebaseDatabase.getInstance ().getReference ().child ("Users");
-        UserProfileImagesRef = FirebaseStorage.getInstance ().getReference ().child ("Profile Images");
+        UsersRef = FirebaseDatabase.getInstance ().getReference ().child ("Users");*/
+        PostImagesRef = FirebaseStorage.getInstance ().getReference ().child ("Profile Images");
 
 
-        UploadPic.setOnClickListener (new View.OnClickListener () {
+        img.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult (intent, 0);
+                Intent intent = new Intent ();
+                intent.setAction (Intent.ACTION_GET_CONTENT);
+                intent.setType ("image/ ");
+                startActivityForResult (intent, GALLERY_PICK);
 
             }
         });
@@ -69,26 +76,68 @@ public class Start_Activity extends AppCompatActivity {
         update.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent (Start_Activity.this,Rating_activity.class);
-                startActivity (i);
+                ValidatePostInfo();
             }
         });
 
 
     }
 
+    private void ValidatePostInfo()
+    {
+            if(ImageUri == null){
+                Toast.makeText (this, "Please Take the photo first", Toast.LENGTH_SHORT).show ();
+
+            }else{
+                StoringImageToFirebase();
+            }
+
+    }
+
+    private void StoringImageToFirebase() {
+
+        Calendar calenderforDate = Calendar.getInstance ();
+        SimpleDateFormat currentDate = new SimpleDateFormat ("dd-MMMM-yyyy");
+        saveCurrentDate = currentDate.format (calenderforDate.getTime ());
+
+        Calendar calenderforTime = Calendar.getInstance ();
+        SimpleDateFormat currentTime = new SimpleDateFormat ("HH:mm");
+        saveCurrentTime = currentTime.format (calenderforTime.getTime ());
+
+        postRandomName = saveCurrentDate + saveCurrentTime;
+
+        StorageReference Filepath =PostImagesRef.child ("Camera picture").child ( ImageUri.getLastPathSegment () + postRandomName );
+
+        Filepath.putFile (ImageUri).addOnCompleteListener (new OnCompleteListener<UploadTask.TaskSnapshot> () {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful ()){
+                    Toast.makeText (Start_Activity.this, "Image Uploaded Successfully ", Toast.LENGTH_SHORT).show ();
+                    Intent intent = new Intent (Start_Activity.this,Rating_activity.class);
+                    startActivity (intent);
+                }else{
+                    String msg = task.getException ().getMessage ();
+                    Toast.makeText (Start_Activity.this, "Error occured" +msg , Toast.LENGTH_SHORT).show ();
+                }
+
+            }
+        });
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult (requestCode, resultCode, data);
 
-        try {
 
-            Bitmap bitmap = (Bitmap) data.getExtras ().get ("data");
-            img.setImageBitmap (bitmap);
+        if(requestCode == GALLERY_PICK && resultCode ==RESULT_OK && data!=null)
+        {
+
+                 ImageUri =data.getData ();
+                img.setImageURI (ImageUri);
+
+
         }
-        catch (Exception e){
-            e.printStackTrace ();
-        }
+
 
     }
 }
