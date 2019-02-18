ff69c17dae44d6e34bcfa6a4ade11df03e76f9e4
Abhiram <abhiramareddy20@gmail.com>
2019-02-18 19:05:32 +0530

    Locks

--- app/src/main/java/com/example/locck/Book.java
+++ app/src/main/java/com/example/locck/Book.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Book extends AppCompatActivity {
 
@@ -19,16 +24,26 @@ public class Book extends AppCompatActivity {
     ImageView imgageView;
     AlertDialog.Builder builder;
     Button b1;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference myref;
+    private String user;
+    int img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_book);
 
+        mAuth = FirebaseAuth.getInstance ();
+        user = mAuth.getCurrentUser ().getUid ();
+        myref = FirebaseDatabase.getInstance ().getReference ().child ("Users").child (user);
+
+
         imgageView =(ImageView)findViewById (R.id.lock1);
         Bundle bundle =getIntent ().getExtras ();
         if(bundle!=null)
         {
-            int img = bundle.getInt ("image");
+             img = bundle.getInt ("image");
             imgageView.setImageResource (img);
 
         }
@@ -40,6 +55,7 @@ public class Book extends AppCompatActivity {
         b1.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
+
 
                 builder.setMessage(R.string.dialog_message) .setTitle(R.string.dialog_title);
 
--- app/src/main/java/com/example/locck/Request_success.java
+++ app/src/main/java/com/example/locck/Request_success.java
@@ -1,28 +1,54 @@
 package com.example.locck;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class Request_success extends AppCompatActivity {
 
     Button b1;
+    private FirebaseAuth mAuth;
+    private String user;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_request_success);
 
+        mAuth = FirebaseAuth.getInstance ();
+        user = mAuth.getCurrentUser ().getUid ();
+        myRef = FirebaseDatabase.getInstance ().getReference ("Users");
+
+
+
         b1 = (Button)findViewById (R.id.next);
 
         b1.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
+
                 startActivity (new Intent (Request_success.this,Start_Activity.class));
             }
         });
 
     }
+
+
+
+
 }
