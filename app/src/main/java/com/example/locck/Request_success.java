cf8a16c4bc0f1a462a7e5a6113f7785227bf31af
Abhiram <abhiramareddy20@gmail.com>
2019-02-19 13:49:03 +0530

    Final Locks

--- app/src/main/java/com/example/locck/Request_success.java
+++ app/src/main/java/com/example/locck/Request_success.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,8 +24,8 @@ public class Request_success extends AppCompatActivity {
 
     Button b1;
     private FirebaseAuth mAuth;
-    private String user;
-    private DatabaseReference myRef;
+    private String SenderId,currentState,reciverId;
+    private DatabaseReference myRef,bookRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +33,48 @@ public class Request_success extends AppCompatActivity {
         setContentView (R.layout.activity_request_success);
 
         mAuth = FirebaseAuth.getInstance ();
-        user = mAuth.getCurrentUser ().getUid ();
-        myRef = FirebaseDatabase.getInstance ().getReference ("Users");
-
-
+        SenderId = mAuth.getCurrentUser ().getUid ();
+        myRef = FirebaseDatabase.getInstance ().getReference ().child ("Users");
+        bookRequest = FirebaseDatabase.getInstance ().getReference ().child ("Users").child (SenderId).child ("Booking Requests");
 
         b1 = (Button)findViewById (R.id.next);
+        currentState = "new";
+
 
         b1.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
 
-                startActivity (new Intent (Request_success.this,Start_Activity.class));
+                if(currentState.equals ("new"))
+                {
+                    SendBookingRequest();
+                }
+                //startActivity (new Intent (Request_success.this,Start_Activity.class));
             }
         });
 
     }
 
+    private void SendBookingRequest() {
+        bookRequest.child ("Request_Type").setValue ("sent")
+                .addOnCompleteListener (new OnCompleteListener<Void> () {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
 
+                        if(task.isSuccessful ())
+                        {
+                            startActivity (new Intent (Request_success.this,Start_Activity.class));
+                            Toast.makeText (Request_success.this, "Booking Confirmed", Toast.LENGTH_SHORT).show ();
+                        }
+                        else
+                        {
+                            Toast.makeText (Request_success.this, "Payment Failed", Toast.LENGTH_SHORT).show ();
+                        }
+
+                    }
+                });
+
+    }
 
 
 }
